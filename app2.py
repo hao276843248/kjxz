282aaccc8760ba3e841046345089f170e4b42fc2
haoxingsen <276843248@qq.com>
2021-03-15 13:15:44 +0800

    日志

--- app2.py
+++ app2.py
@@ -2,6 +2,7 @@ import json
 
 import requests
 from flask import Flask, request, jsonify, render_template, current_app
+import logging
 
 app = Flask(__name__)
 
@@ -22,27 +23,31 @@ def passHtml():
     url = request.json.get("url")
     if not url:
         return jsonify([])
-    # http://www.tantanjiujiu.com/tantanjiujiu/bookinfo/showkejian?type=LXYYZW&nianji=3X&danyuan=2&kewenxuhao=1&id=4670
-    url = url.split("?")[1]
-    urls = url.split("&")
-    urls = list(filter(lambda i: "id" in i, urls))[0]
-    ids = urls.split("=")[1]
     rets = []
-    r2 = requests.post(
-        "http://www.tantanjiujiu.com/tantanjiujiu/bookinfo/jiancha",
-        data={"kejianurl": ids}
-    )
-    allData = json.loads(r2.text)
-    url = "http://www.tantanjiujiu.com"
-    if allData["kejianurl"]:
-        rets.append({"name": "课件", "url": url + allData["kejianurl"]})
-    if allData["jiaoxuesheji"]:
-        rets.append({"name": "教学设计", "url": url + allData["jiaoxuesheji"]})
-    if allData["xbpaper"]:
-        rets.append({"name": "学霸小测", "url": allData["xbpaper"]})
-    if not rets:
-        rets.append({"name": "解析错误", "url": ""})
-    print(rets)
+    try:
+        # http://www.tantanjiujiu.com/tantanjiujiu/bookinfo/showkejian?type=LXYYZW&nianji=3X&danyuan=2&kewenxuhao=1&id=4670
+        url = url.split("?")[1]
+        urls = url.split("&")
+        urls = list(filter(lambda i: "id" in i, urls))[0]
+        ids = urls.split("=")[1]
+        r2 = requests.post(
+            "http://www.tantanjiujiu.com/tantanjiujiu/bookinfo/jiancha",
+            data={"kejianurl": ids}
+        )
+        allData = json.loads(r2.text)
+        url = "http://www.tantanjiujiu.com"
+        current_app.logger.inof(allData)
+        if allData["kejianurl"]:
+            rets.append({"name": "课件", "url": url + allData["kejianurl"]})
+        if allData["jiaoxuesheji"]:
+            rets.append({"name": "教学设计", "url": url + allData["jiaoxuesheji"]})
+        if allData["xbpaper"]:
+            rets.append({"name": "学霸小测", "url": allData["xbpaper"]})
+        if not rets:
+            rets.append({"name": "解析错误", "url": ""})
+        current_app.logger.inof(rets)
+    except Exception as e:
+        current_app.logger.error(e)
     return jsonify(rets)
 
 
